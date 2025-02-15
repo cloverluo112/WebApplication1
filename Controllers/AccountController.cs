b120e2d0515e6d6898528593040d6c19a007ad17
cloverluo112 <110218255+cloverluo112@users.noreply.github.com>
2025-02-16 02:03:57 +1030

    Create Login Method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -21,6 +21,24 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email or passowrd is incorrect.");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
         public IActionResult Register()
         {
             return View();
